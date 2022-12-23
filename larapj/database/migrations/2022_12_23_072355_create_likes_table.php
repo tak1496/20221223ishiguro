6dd4b02887908a89b54297fa88655ea17a661e87
tak1496 <tak.it.easy1@gmail.com>
2022-12-23 16:27:02 +0900

    実践モダン開発 演習問題

---
+++ larapj/database/migrations/2022_12_23_072355_create_likes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id')->nullable(false);
+            $table->integer('user_id')->nullable(false);
+            $table
+                ->timestamp('created_at')
+                ->useCurrent()
+                ->nullable();
+            $table
+                ->timestamp('updated_at')
+                ->useCurrent()
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
